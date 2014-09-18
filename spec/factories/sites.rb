d809fc9fc097bc93ecd76c2c3dec41e215ff8d59
Prelang Builder <builder@prelang.com>
2014-09-18 01:28:10 +0000

    Generated model Site with migration

---
+++ spec/factories/sites.rb
@@ -0,0 +1,11 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :site do
+    city "MyString"
+    site_id "MyString"
+    state "MyString"
+    street "MyString"
+    ce_email "MyString"
+  end
+end
